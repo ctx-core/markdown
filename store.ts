38db9a1e2eb7092bc0d2fee8365189c36ee702f9
Brian Takita <brian.takita@gmail.com>
2020-09-11 19:06:16 -0700

    import type for type imports

--- store.ts
+++ store.ts
@@ -1,5 +1,5 @@
-import { writable } from 'svelte/store'
 import { _b } from '@ctx-core/object'
+import { writable } from 'svelte/store'
 export const b__frontmatter = _b('__frontmatter', ()=>
 	writable(null))
 export const __frontmatter = b__frontmatter()
