051110db47888932c9773f58d9881b5087ecb233
Brian Takita <brian.takita@gmail.com>
2021-03-28 18:14:12 -0400

    update dependencies

--- src/markdown_frontmatter_b.d.ts
+++ src/markdown_frontmatter_b.d.ts
@@ -1,2 +1,2 @@
-export declare const markdown_frontmatter_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Writable<null>, object>;
+export declare const markdown_frontmatter_b: import("@ctx-core/object").Be<import("@ctx-core/store").Writable<null>, object>;
 export { markdown_frontmatter_b as b__frontmatter };
