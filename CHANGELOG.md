cda3f3ee4b6c89e7d5f08982b6b9329ccc126606
Brian Takita <brian.takita@gmail.com>
2021-05-31 15:57:14 -0400

    - \be_: required ctx argument & generic type updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 # @ctx-core/markdown
 
+## 11.0.4
+
+### Patch Changes
+
+- _b->be_
+- Updated dependencies [undefined]
+  - @ctx-core/array@21.0.4
+  - @ctx-core/object@19.0.2
+
 ## 11.0.3
 
 ### Patch Changes
@@ -1301,7 +1310,7 @@
 
 ### Minor Changes
 
-- \_b: required ctx argument & generic type updates
+- \be\_: required ctx argument & generic type updates
 - Extract separate modules
 
 ### Patch Changes
--- src/markdown_frontmatter$_b.ts
+++ src/markdown_frontmatter$_b.ts
@@ -1,7 +1,7 @@
-import { _b } from '@ctx-core/object'
+import { be_ } from '@ctx-core/object'
 import { writable$, Writable$ } from '@ctx-core/store'
 import type { markdown_Ctx } from './markdown_Ctx'
 const key = 'markdown_frontmatter$'
-export const markdown_frontmatter$_b = _b<markdown_Ctx, typeof key>(key, ()=>
+export const markdown_frontmatter$_b = be_<markdown_Ctx, typeof key>(key, ()=>
 	writable$(undefined))
 export type markdown_frontmatter$_T = Writable$<string|undefined>
